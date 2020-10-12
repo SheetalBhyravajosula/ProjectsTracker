b91effe3ebdcc619a298e033e89dff1c08367912
Sharada <shardachanda@gmail.com>
2020-10-12 23:00:19 +0530

    added task and tasktype schema

--- schemas/tasksSchema.js
+++ schemas/tasksSchema.js
@@ -1,16 +1,34 @@
-var mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-var Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var TaskSchema=new Schema({
-    TaskID:String,
-    TaskDescription:String,
-    TaskTypeID:String,
-    ProjectID:String,
-    EmpID:String,
-    TaskStartDate:String,
-    TaskEndDate:String,
-    Duration:String
-},{collection:'TaskCollection'});
+const TaskSchema = new Schema({    
+    TaskDescription: {
+                        type :String,
+                        required : true,
+                        trim : true
+                     },
+    TaskType: {
+                type : Number,
+                required : true,
+                ref : 'TaskType'
+              },
+    Project: {
+                type : mongoose.Schema.Types.ObjectId,
+                required : true,                
+                ref : 'Project'
+             },
+    Employee : {
+                 type : String,
+                 required : true,
+                 ref : 'Employee'
+               },
+    TaskStartDate:Date,
+    TaskEndDate:Date,
+    Duration:mongoose.Types.Decimal128
+}, {
+    timestamps : true,
+    collection : 'Task'
+});
 
-module.exports=mongoose.model('TaskCollection',TaskSchema);
+module.exports = mongoose.model('Task',TaskSchema);
